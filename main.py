cd985008ed35c89a65df19da33ef231da3c303fd
Francesco Silvani <83598456+Totallynotnasaboi@users.noreply.github.com>
2021-09-01 13:26:50 +0000

    Adress Bar now changes if you change website (back or forward)

--- main.py
+++ main.py
@@ -36,12 +36,17 @@ class MainWindow(QMainWindow):
     self.url_bar.returnPressed.connect(self.navigate_to_url)
     navbar.addWidget(self.url_bar)
 
+    self.browser.urlChanged.connect(self.update_url)
+
   def navigate_home(self):
     self.browser.setUrl(QUrl('https://nasa-boi.ga'))
   
   def navigate_to_url(self):
     url = self.url_bar.text()
     self.browser.setUrl(QUrl(url))
+  
+  def update_url(self, q):
+    self.url_bar.setText(q.ToString())
 
 
 app = QApplication(sys.argv)
