54ea0a8caaa4343a9fe69c92f9fe44fafc374e9b
Francesco Silvani <83598456+Totallynotnasaboi@users.noreply.github.com>
2021-09-01 13:14:27 +0000

    Added navbar (no url navigator)

--- main.py
+++ main.py
@@ -12,6 +12,23 @@ class MainWindow(QMainWindow):
     self.setCentralWidget(self.browser)
     self.showMaximized()
 
+    #navbar
+    navbar = QToolBar()
+    self.addToolBar(navbar)
+
+    back_btn = QAction('Back', self)
+    back_btn.triggered.connect(self.browser.back)
+    navbar.addAction(back_btn)
+
+    forward_btn = QAction('Forward', self)
+    forward_btn.triggered.connect(self.browser.forward)
+    navbar.addAction(forward_btn)
+
+    reload_btn = QAction('Reload', self)
+    reload_btn.triggered.connect(self.browser.reload)
+    navbar.addAction(reload_btn)
+
+
 app = QApplication(sys.argv)
 QApplication.setApplicationName('Nasa Boi Browser')
 QApplication.setApplicationVersion('1.0.0')
